aa36374bad951230a5221089d2b6f4e74b39c0f4
lordstanleyfour <69077577+lordstanleyfour@users.noreply.github.com>
2022-05-27 14:15:46 +0100

    Update main.js

--- sidescroller/main.js
+++ sidescroller/main.js
@@ -3,7 +3,7 @@ import {InputHandler} from './input.js';
 import {Background} from './background.js';
 import {FlyingEnemy, ClimbingEnemy, GroundEnemy} from './enemies.js';
 import {UI} from './UI.js';
-import defaultExport from './loopify.js';
+import {loopify} from './loopify.js';
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
