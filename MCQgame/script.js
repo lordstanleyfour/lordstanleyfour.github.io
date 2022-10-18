79be797970763046825e92706598ad99b69af365
lordstanleyfour <69077577+lordstanleyfour@users.noreply.github.com>
2022-10-18 19:36:51 +0100

    Update script.js

--- MCQgame/script.js
+++ MCQgame/script.js
@@ -25,9 +25,11 @@ function collision (object){
     }    
 }
 
-var rng = Math.floor(Math.random()*(4-1+1)+1);
+var rng = Math.floor(Math.random()*3);
+var questionArraySelected = undefined;
+var correctAnswerSelected = false;
 
-let questions = [
+let defaultQuestions = [
     {
         question: "A",
         correctAnswer: "1",
@@ -44,60 +46,145 @@ let questions = [
     }
 ];
 
-let positionArray =[{x:50,y:150}, {x:150,y:150}, {x:50,y:250}, {x:150,y:250}]; 
+let alternativeQuestions = [
+    {
+        question: "AA",
+        correctAnswer: "11",
+        altAnswer1: "22",
+        altAnswer2: "33",
+        altAnswer3: "44"
+    },
+    {
+        question: "BB",
+        correctAnswer: "55",
+        altAnswer1: "66",
+        altAnswer2: "77",
+        altAnswer3: "88"
+    }
+];
+
+let questionBoxPositionArray =[{x:50,y:150}, {x:150,y:150}, {x:50,y:250}, {x:150,y:250}]; 
 
 class TargetBox {
-    constructor(x, y, w, h, text){
+    constructor(x, y, purposeSelect, category){
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
-        this.text = text;
+        this.w = 50;
+        this.h = 50;
+        this.text = undefined;
+        this.purposeSelect = purposeSelect;
+        this.category = category;
         this.correct = undefined;
         this.position = undefined;
     }
     draw(){
         ctx.fillStyle = 'green';
         ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.textAlign = 'center';
         ctx.strokeText(this.text, this.x + (this.w/2), this.y + (this.h/2));
     }
     update(){
+        //for question boxes
         if (this.position) {
             this.x = this.position.x;
             this.y = this.position.y
-        } else {
-            this.x = 0; //find a better default for the boxes?
-            this.y - 0;
         }
-        if (collision(this)) console.log('clicked');       
+        if (questionArraySelected){
+            if (this.purposeSelect === 0) {
+                this.text = questionArraySelected[0].correctAnswer;
+                this.correct = true;
+            } else if (this.purposeSelect === 1) {
+                this.text = questionArraySelected[0].altAnswer1;
+                this.correct = false;
+            }
+            else if (this.purposeSelect === 2) {
+                this.text = questionArraySelected[0].altAnswer2;
+                this.correct = false;
+            }
+            else if (this.purposeSelect === 3) {
+                this.text = questionArraySelected[0].altAnswer3;
+                this.correct = false;
+            }
+        }
+
+        //for category boxes
+        if (this.purposeSelect === 4){
+            if (this.category === 0) this.text = "defaultQuestions";
+            else if (this.category === 1) this.text = "alternativeQuestions";
+        }
+	
+        if (collision(this) && this.correct) {
+            console.log('correct');
+            correctAnswerSelected = true;
+        } else if (collision(this) && this.correct === false){
+            console.log('false');
+        } else if (collision(this) && this.correct === undefined){
+            if (this.category === 0){
+                questionArraySelected = defaultQuestions;
+            } else if (this.category === 1){
+                questionArraySelected = alternativeQuestions;
+            }
+        }
     }
 }
 
-var boxA = new TargetBox(undefined, undefined, 50, 50, questions[0].correctAnswer);
-var boxB = new TargetBox(undefined, undefined, 50, 50, questions[0].altAnswer1);
-var boxC = new TargetBox(undefined, undefined, 50, 50, questions[0].altAnswer2);
-var boxD = new TargetBox(undefined, undefined, 50, 50, questions[0].altAnswer3);
+var questionBox1 = new TargetBox(undefined, undefined, 0);
+var questionBox2 = new TargetBox(undefined, undefined, 1);
+var questionBox3 = new TargetBox(undefined, undefined, 2);
+var questionBox4 = new TargetBox(undefined, undefined, 3);
+let questionBoxArray = [questionBox1, questionBox2, questionBox3, questionBox4];
 
-let boxArray = [boxA, boxB, boxC, boxD];
+var categoryBox1 = new TargetBox(500, 150, 4, 0);
+var categoryBox2 = new TargetBox(600, 150, 4, 1);
+let categoryBoxArray = [categoryBox1, categoryBox2];
+
+
+
+function categoryHandler(){
+    //if no category selected then display text prompting a choice
+
+}
+
+
+
+
 
 function questionHandler(){
-    ctx.strokeText("THIS IS WHERE THE QUESTIONS LIVE", 50, 50);
-    ctx.strokeText(questions[0].question, 50, 75);
+
+    if (questionArraySelected){
+        ctx.strokeText("THIS IS WHERE THE QUESTIONS LIVE", 100, 50);
+        ctx.strokeText(questionArraySelected[0].question, 50, 75);
+    }
+
+
+    if (correctAnswerSelected){
+        questionArraySelected.splice(0, 1);
+        correctAnswerSelected = false;
+        if (questionArraySelected.length === 0){
+            alert("Hooray.");
+        }
+    }
 }
 
 function boxHandler(){
-    for (i = 0; i < boxArray.length; i++){
-        boxArray[i].x = 10+ 75*i;
-        boxArray[i].y = 100;
-        //positionArray index i + rng sum
+    for (i = 0; i < questionBoxArray.length; i++){
+	    if ((rng + i) <= 3){
+        	questionBoxArray[i].position = questionBoxPositionArray[rng + i];
+	    } else if ((rng + i) > 3) {
+		questionBoxArray[i].position = questionBoxPositionArray[((rng+i)-4)];
+    	    }        
     }
-    boxArray.forEach(element => {
+    questionBoxArray.forEach(element => {
         element.draw();
-        element.update();
-    });
+        element.update();    
+    })
+    categoryBoxArray.forEach(element => {
+        element.draw();
+        element.update();  
+    })
 }
 
-
+	
 
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
@@ -118,11 +205,12 @@ function animate(){
         then = now - (elapsed % fpsInterval);
         ctx.clearRect(0, 0, canvas1.width, canvas1.height);
 
-        //rng = Math.floor(Math.random()*(4-1+1)+1); Put this wherever the reset ends up
-        console.log(rng);
+        //rng = Math.floor(Math.random()*3); Put this wherever the reset ends up
+
 
         questionHandler();
-        boxHandler();
+	    boxHandler();
+
 
 
         //reset mouse
@@ -134,9 +222,11 @@ function animate(){
 }
 startAnimating(15);
 
+//move the category box logic into the category handler function and refactor instructions
+//shuffle question array logic in category handler function
+//make more category boxes (5 needed)
+//for loop in box handler to place category boxes in position
+//if question array empty then splice category box
+//default (category not selected) behaviour for question box text
 
-
-////declare fixed positions 1-4 for the boxes
-////box A with the correct answer gets positionRNG
-////boxes B-D get assigned progressive positions from positionRNG
-////"IF rng >=4 then box goes to position (rng+1)-3"
+//flow -> select category prompt -> category selected -> question boxes displayed -> reselect category on correct
